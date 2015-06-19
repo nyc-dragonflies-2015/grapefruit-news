a4cdc4170877dba7e4138599c892bbd71b830340
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-18 22:24:16 -0400

    updated the userwrotethispost to look for a signed-in user first

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def the_signed_in_user
     return User.find_by(id: session[:user_id])
   end
+
+  def the_user_wrote_this_post(post)
+    if session[:user_id]
+      return true if the_signed_in_user.id == post.user.id
+    end
+  end
 end
