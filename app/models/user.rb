251f4b15a460e2f2f717b9b43fa77c4657fc05bb
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-19 12:25:57 -0400

    updated model with postvotes and commentvotes assoc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :post_votes
+  has_many :comment_votes
   has_many :comments
   has_many :posts
   validates :username, presence: true, uniqueness: true
