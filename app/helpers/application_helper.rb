3ad020adc5c974e7a464cf86608caf5c40d67584
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-19 12:23:41 -0400

    made two helper methods to check a vote for users and posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,11 @@ module ApplicationHelper
   def the_user_is_signed_in
     return true if session[:user_id]
   end
+
+  def user_has_voted_for_post(post)
+    return true if session[:user_id] && post.post_votes.find_by(user_id: the_signed_in_user.id)
+  end
+  def the_user_and_posts_vote(post)
+    PostVote.find_by(user_id: the_signed_in_user.id, post_id: post.id)
+  end
 end
