e4047f58659beb39926e99f8e03a30c098ea2780
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-19 10:35:46 -0400

    Added logic to my update and destroy paths

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,16 +19,23 @@ class CommentsController < ApplicationController
   def edit
     @comment = Comment.find_by(id: params[:id])
     @post = Post.find_by(id: params[:post_id])
-    binding.pry
   end
 
   def update
-    p params
-    render plain: "update comment!!!"
+    @comment = Comment.find_by(id: params[:id])
+    @comment.assign_attributes(comment_params)
+    if @comment.save
+      redirect_to post_path(params[:post_id])
+    else
+      flash[:error] = "There was a problem with your comment" #need to make this clearer or possibly just update attributes since there isn't much that can go wrong with a comment.
+      redirect_to :back
+    end
   end
 
   def destroy
-    render plain: "destroy comment!!!"
+    post = Post.find_by(id: params[:post_id])
+    Comment.find_by(id: params[:id]).destroy
+    redirect_to post
   end
 
   private
