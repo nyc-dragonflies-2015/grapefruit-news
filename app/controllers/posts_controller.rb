099900a8f7649a11408856a5e4751308bdce1a8c
Ross Wilson <arthur.ross.wilson@gmail.com>
2015-06-18 22:17:01 -0400

    created update and destroy route
    I made my update edit and destroy routes
    I made a nillify method that will make blank inputs nil in the post submission
    updated my show route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
     if post_has_content_and_url?
       redirect_to :back
     else
+      nillify_blanks!
       @post = Post.new(post_params)
       if @post.save
         redirect_to @post
@@ -23,20 +24,40 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
-    if @post.url
-      redirect_to "#{@post.url}"
+    if @post
+      if @post.url
+        redirect_to "#{@post.url}"
+      else
+        render
+      end
     else
-      render
+      redirect_to root_path
     end
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
   end
 
   def update
+    @post = Post.find_by(id: params[:id])
+    if post_has_content_and_url?
+      redirect_to :back
+    else
+      nillify_blanks!
+      @post.assign_attributes(post_params)
+      if @post.save
+        redirect_to @post
+      else
+        redirect_to :back
+      end
+    end
   end
 
-  def delete
+  def destroy
+    @user = User.find_by(id: session[:user_id])
+    Post.find_by(id: params[:id]).destroy
+    redirect_to user_posts_path(@user)
   end
 
   private
@@ -50,6 +71,12 @@ class PostsController < ApplicationController
       return true
     end
   end
+
+  def nillify_blanks!
+    params[:post][:url] = nil if post_params[:url] == ""
+    params[:post][:content] = nil if post_params[:content] == ""
+  end
+
 
   def is_user_logged_in?
     return true if session[:user_id]
